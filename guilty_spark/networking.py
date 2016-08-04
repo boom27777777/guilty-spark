30c7e6e6aafd02b28b1566f31282c1db6fc1f16c
Boom <boom@mouse.fail>
2016-08-03 22:23:35 -0700

    Doing some house keeping

--- guilty_spark/networking.py
+++ guilty_spark/networking.py
@@ -2,17 +2,25 @@ import logging
 import urllib.request as requests
 
 
-def fetch_page(url):
+def fetch_page(url, method='GET', data=None, headers=None):
     """ Builds a request as the bot to request anything through HTTP
 
     :param url:
         The url to fetch
+    :param method:
+        Http method to use
+    :param data:
+        Data for POST
+    :param headers:
+        HTTP Headers for a urllib.request.Request object
     :return:
         Response content or None if an error occurs
     """
     content = None
     try:
-        request = requests.Request(url)
+        request = requests.Request(url, method=method, data=data,
+                                   headers=headers)
+
         opener = requests.build_opener()
         request.add_header(
             'User-Agent', 'GuiltySpark/1.0 (/u/CheetElwin)')
