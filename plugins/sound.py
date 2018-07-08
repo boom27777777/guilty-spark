41b8712f5b3a828bac1d105aede76ad46ca5f41e
Boom <boom@mouse.fail>
2018-07-08 01:17:47 -0700

    Adds volume control to sound manager

--- plugins/sound.py
+++ plugins/sound.py
@@ -22,6 +22,36 @@ class Sound(Plugin):
 
         await self.bot.send_embed(embed)
 
+    async def set_volume(self, to_set):
+        try:
+            new_vol = float(to_set) / 100.0
+            if new_vol > 1.0 or new_vol < 0.0:
+                raise ValueError
+
+            self.bot.set_volume(new_vol)
+        except ValueError:
+            await self.bot.send_embed(self.build_embed(
+                title='Error',
+                description='Failed to set Volume to {} '
+                            'only 0-100 allowed'.format(to_set),
+                level=2
+            ))
+
     async def on_command(self, command, message: discord.Message):
-        if 'stop' in message.content:
+        _, *args = message.content.split()
+
+        if len(args) == 0:
+            await self.help(message)
+
+        elif args[0] == 'stop':
             await self.bot.stop_sound(message.author)
+
+        elif args[0] == 'volume':
+            if len(args) == 2:
+                await self.set_volume(args[1])
+            else:
+                await self.bot.send_embed(self.build_embed(
+                    title='Sound Volume',
+                    description='```{:.0}/100```'.format(
+                        self.bot.get_volume() * 100.0)
+                ))
