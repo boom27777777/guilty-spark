1a0e769db35b8132b7b0ec7689de175ab766b8a8
Jackson McCrea <jam@rampartdetection.com>
2018-10-09 20:00:54 -0700

    Adds a THICC group management plugin

---
+++ plugins/thicc_role.py
@@ -0,0 +1,67 @@
+import discord
+
+from guilty_spark.plugin_system.plugin import Plugin
+
+
+class THICCRole(Plugin):
+    def __init__(self, name: str, bot):
+        super().__init__(name, bot, commands=['thicc'])
+
+        self.use_whitelist = True
+
+    async def help(self, _):
+        embed = self.build_embed(
+            title='THICC Group management',
+            description='Auto \n\n'
+                        '**Usage**: `{}thicc [group_name] [@user]`\n\n'.format(
+                self.bot.prefix)
+        )
+        await self.bot.send_embed(embed)
+
+    @Plugin.admin
+    async def on_command(self, command, message: discord.Message):
+        try:
+            _, *args = message.content.split()
+            if len(args) < 2:
+                raise IndexError
+        except IndexError:
+            await self.help(command)
+            return
+
+        name = 'THICC#{}'.format(args[0])
+        user = args[1].replace('<', '').replace('@', '').replace('>', '')
+
+        try:
+            role = await self.bot.create_role(
+                server=message.server,
+                name=name,
+                mentionable=True
+            )
+        except discord.Forbidden:
+            await self.bot.send_embed(self.build_embed(
+                title='Error',
+                description='I do not have permissions to make new roles',
+                level=2
+            ))
+            return
+
+        try:
+            await self.bot.add_roles(
+                self.bot.get_user_by_id(user),
+                role
+            )
+        except discord.Forbidden:
+            await self.bot.send_embed(self.build_embed(
+                title='Error',
+                description='I do not have permissions to add roles to user',
+                level=2
+            ))
+            return
+
+        await self.bot.send_embed(self.build_embed(
+            title="Success",
+            description="Role {} Created and assinged to <@{}>".format(
+                name,
+                user
+            )
+        ))
